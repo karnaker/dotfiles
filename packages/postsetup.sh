f105def48d7b74d3670097303b49dad01138f999
Vikram Karnaker <vkarnaker@gmail.com>
2022-11-16 20:47:40 -0500

    feat: Configure Xcode and sign license

--- packages/postsetup.sh
+++ packages/postsetup.sh
@@ -10,8 +10,12 @@ brew update && brew upgrade
 success "Finished updating and upgrading Homebrew."
 
 info "Installing Ruby gems..."
-
 gem install jekyll
 gem install bundler
-
 success "Finished installing Ruby gems."
+
+info "Configure Xcode command-line tools to use the newly-installed version of Xcode..."
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -runFirstLaunch
+sudo xcodebuild -license
+success "Configure Xcode command-line tools to use the newly-installed version of Xcode."
