f4586bc71a5cc3b1f22b13cf6e4580986def6e41
Vikram Karnaker <vkarnaker@gmail.com>
2023-08-17 23:43:32 -0400

    chore: comment out docutil lines

--- macos/setup.sh
+++ macos/setup.sh
@@ -473,16 +473,16 @@ defaults write com.apple.dock wvous-br-corner   -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
 
 # docutil
-dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app" --position 1
-dockutil --no-restart --add "/Applications/Discord.app" --position 2
-dockutil --no-restart --add "/Applications/ExpressVPN.app" --position 3
-dockutil --no-restart --add "/Applications/iTerm.app" --position 4
-dockutil --no-restart --add "/Applications/MongoDB Compass.app" --position 5
-dockutil --no-restart --add "/Applications/Slack.app" --position 6
-dockutil --no-restart --add "/Applications/Visual Studio Code.app" --position 7
-killall Dock
-info "Dock is set..."
+# dockutil --no-restart --remove all
+# dockutil --no-restart --add "/Applications/Google Chrome.app" --position 1
+# dockutil --no-restart --add "/Applications/Discord.app" --position 2
+# dockutil --no-restart --add "/Applications/ExpressVPN.app" --position 3
+# dockutil --no-restart --add "/Applications/iTerm.app" --position 4
+# dockutil --no-restart --add "/Applications/MongoDB Compass.app" --position 5
+# dockutil --no-restart --add "/Applications/Slack.app" --position 6
+# dockutil --no-restart --add "/Applications/Visual Studio Code.app" --position 7
+# killall Dock
+# info "Dock is set..."
 
 ###############################################################################
 # Safari & WebKit                                                             #
