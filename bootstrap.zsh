feffcce4eb81c35e6baafaaead0b4cbe36f1b47d
Vikram Karnaker <vikram@vmbp.myfiosgateway.com>
2023-10-07 20:14:54 -0400

    Debug: Install packages using homebrew

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -18,9 +18,25 @@ echo -e "\033[1;36m==== Upgrading Installed Formulae ====\033[0m"
 brew upgrade
 
 # Install specific packages and software (only if they aren't already installed)
-echo -e "\033[1;36m==== Installing Specified Packages and Software ====\033[0m"
-brew install heroku mongodb mas node redis coreutils git
-brew install --cask 1password acrobat-pro acrobat-reader android-studio discord docker exodus expressvpn flutter google-chrome iterm2 ledger-live mongodb-compass ngrok paintbrush rectangle slack visual-studio-code vlc zoom
+
+# Tap necessary repositories for specific formulae
+echo -e "\033[1;36m==== Tapping Necessary Repositories ====\033[0m"
+brew tap heroku/brew
+brew tap mongodb/brew
+
+# Install cask software
+echo -e "\033[1;36m==== Installing Cask Software ====\033[0m"
+brew install --cask 1password adobe-acrobat-pro adobe-acrobat-reader android-studio discord docker exodus expressvpn flutter google-chrome google-drive iterm2 krita ledger-live mongodb-compass ngrok paintbrush rectangle slack visual-studio-code vlc zoom
+
+# Install formulae
+echo -e "\033[1;36m==== Installing Formulae ====\033[0m"
+brew install cocoapods coreutils git heroku mas mongodb-community node redis
+
+# Install software from the Mac App Store using mas
+echo -e "\033[1;36m==== Installing Mac App Store Software ====\033[0m"
+mas install 682658836 # GarageBand
+mas install 408981434 # iMovie
+mas install 497799835 # Xcode
 
 # Inform the user that the bootstrap process is complete
 echo -e "\n\033[1;36m==== Bootstrap Process Complete! ====\033[0m\n"
