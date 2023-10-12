a80af7f494cdbdd6d699a171254988ab1bce0846
Vikram Karnaker <vikram@vmbp.myfiosgateway.com>
2023-10-11 22:35:09 -0400

    Refactor: Modularize functions, enhance error handling, and improve documentation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env sh
 
-# Use formatting to make the beginning of the process stand out
-echo -e "\n\033[1;36m==== Preparing to Run Bootstrap ====\033[0m\n"
+# Main bootstrap script
 
-# Grant execute permissions to bootstrap.sh
-echo -e "\033[1;36m==== Granting Execute Permissions to bootstrap.sh ====\033[0m"
-chmod +x bootstrap.sh
+# Function to inform the user about the bootstrap start
+start_bootstrap() {
+    echo -e "\n\033[1;36m==== Starting the Bootstrap Process ====\033[0m\n"
+}
 
-# Run bootstrap.sh
-echo -e "\033[1;36m==== Running bootstrap.sh ====\033[0m"
-./bootstrap.sh
+# Function to grant execute permissions to scripts
+grant_permissions() {
+    echo -e "\033[1;36m==== Granting Execute Permissions to Scripts in ./scripts ====\033[0m"
+    chmod +x scripts/*.sh
+}
 
-# Use formatting to indicate the end of the process
-echo -e "\n\033[1;36m==== Process Complete! ====\033[0m\n"
+# Function to run the package installation script
+run_install_packages() {
+    echo -e "\033[1;36m==== Running Package Installation Script ====\033[0m"
+    if [ -x scripts/install_packages.sh ]; then
+        sh scripts/install_packages.sh
+    else
+        echo -e "\033[1;31mError: scripts/install_packages.sh does not have execute permissions or does not exist.\033[0m"
+        exit 1
+    fi
+}
+
+# Function to inform the user about the bootstrap completion
+end_bootstrap() {
+    echo -e "\n\033[1;36m==== Bootstrap Process Complete! ====\033[0m\n"
+}
+
+# Main function to run the bootstrap process
+main() {
+    start_bootstrap
+    grant_permissions
+    run_install_packages
+    end_bootstrap
+}
+
+# Call the main function
+main
--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,42 +1,70 @@
 #!/usr/bin/env sh
 
-# Inform the user that the bootstrap process is starting
-echo -e "\n\033[1;36m==== Starting the Bootstrap Process ====\033[0m\n"
+# This script handles the installation of software packages
 
-# Check if Homebrew is installed. If it's not, install it.
-if [ ! $(which brew) ]; then
-  echo -e "\033[1;36m==== Installing Homebrew ====\033[0m"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+# Function to install Homebrew
+install_homebrew() {
+    if [ ! $(which brew) ]; then
+        echo -e "\033[1;36m==== Installing Homebrew ====\033[0m"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+}
 
-# Update the Homebrew recipes to their latest versions
-echo -e "\033[1;36m==== Updating Homebrew Recipes ====\033[0m"
-brew update
+# Function to update Homebrew
+update_homebrew() {
+    echo -e "\033[1;36m==== Updating Homebrew Recipes ====\033[0m"
+    brew update
+}
 
-# Upgrade all installed formulae to their latest versions
-echo -e "\033[1;36m==== Upgrading Installed Formulae ====\033[0m"
-brew upgrade
+# Function to upgrade formulae
+upgrade_formulae() {
+    echo -e "\033[1;36m==== Upgrading Installed Formulae ====\033[0m"
+    brew upgrade
+}
 
-# Install specific packages and software (only if they aren't already installed)
+# Function to tap repositories
+tap_repositories() {
+    echo -e "\033[1;36m==== Tapping Necessary Repositories ====\033[0m"
+    brew tap heroku/brew
+    brew tap mongodb/brew
+}
 
-# Tap necessary repositories for specific formulae
-echo -e "\033[1;36m==== Tapping Necessary Repositories ====\033[0m"
-brew tap heroku/brew
-brew tap mongodb/brew
+# Function to install cask software
+install_cask_software() {
+    echo -e "\033[1;36m==== Installing Cask Software ====\033[0m"
+    brew install --cask 1password adobe-acrobat-pro adobe-acrobat-reader android-studio discord docker exodus expressvpn flutter google-chrome google-drive iterm2 krita ledger-live mongodb-compass ngrok paintbrush rectangle slack visual-studio-code vlc zoom
+}
 
-# Install cask software
-echo -e "\033[1;36m==== Installing Cask Software ====\033[0m"
-brew install --cask 1password adobe-acrobat-pro adobe-acrobat-reader android-studio discord docker exodus expressvpn flutter google-chrome google-drive iterm2 krita ledger-live mongodb-compass ngrok paintbrush rectangle slack visual-studio-code vlc zoom
+# Function to install formulae
+install_formulae() {
+    echo -e "\033[1;36m==== Installing Formulae ====\033[0m"
+    brew install cocoapods coreutils git heroku mas mongodb-community node redis
+}
 
-# Install formulae
-echo -e "\033[1;36m==== Installing Formulae ====\033[0m"
-brew install cocoapods coreutils git heroku mas mongodb-community node redis
+# Function to install MAS software
+install_mas_software() {
+    echo -e "\033[1;36m==== Installing Mac App Store Software ====\033[0m"
+    mas install 682658836 # GarageBand
+    mas install 408981434 # iMovie
+    mas install 497799835 # Xcode
+}
 
-# Install software from the Mac App Store using mas
-echo -e "\033[1;36m==== Installing Mac App Store Software ====\033[0m"
-mas install 682658836 # GarageBand
-mas install 408981434 # iMovie
-mas install 497799835 # Xcode
+# Main function to orchestrate the package installation
+install_packages() {
+    # Inform the user that the installation process is starting
+    echo -e "\n\033[1;36m==== Starting the Package Installation Process ====\033[0m\n"
+    
+    install_homebrew
+    update_homebrew
+    upgrade_formulae
+    tap_repositories
+    install_cask_software
+    install_formulae
+    install_mas_software
 
-# Inform the user that the bootstrap process is complete
-echo -e "\n\033[1;36m==== Bootstrap Process Complete! ====\033[0m\n"
+    # Inform the user that the installation process is complete
+    echo -e "\n\033[1;36m==== Package Installation Complete! ====\033[0m\n"
+}
+
+# Call the main function
+install_packages
