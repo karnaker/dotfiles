10c95c33ac032db336f1a714148565060f0fc484
Vikram Karnaker <vkarnaker@gmail.com>
2023-10-19 11:51:46 -0400

    Add new .zshrc and setup_zsh_configs.sh script, and run that using bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,20 @@
 
 # Main bootstrap script
 
+# Colors for printing
+CYAN="\033[1;36m"
+RESET="\033[0m"
+
 # Function to inform the user about the bootstrap start
 start_bootstrap() {
-    printf "\n\033[1;36m==== Starting the Bootstrap Process ====\033[0m\n"
+    printf "\n${CYAN}==== Starting the Bootstrap Process ====${RESET}\n"
 }
 
 # Function to prompt for sudo password at the beginning
 prompt_for_password() {
     # Check if user is not root and then prompt for password
     if [ "$EUID" -ne 0 ]; then
-        printf "\033[1;36mPlease enter your password for script execution\033[0m\n"
+        printf "${CYAN}Please enter your password for script execution${RESET}\n"
         sudo -v
         
         # Keep updating the sudo timestamp to avoid asking password again during execution
@@ -21,81 +25,93 @@ prompt_for_password() {
 
 # Function to grant execute permissions to scripts
 grant_permissions() {
-    printf "\033[1;36m==== Granting Execute Permissions to Scripts in ./scripts ====\033[0m\n"
+    printf "${CYAN}==== Granting Execute Permissions to Scripts in ./scripts ====${RESET}\n"
     chmod +x scripts/*.sh
 }
 
 # Function to run the system tools installation script
 run_system_tools() {
-    printf "\033[1;36m==== Running System Tools Installation Script ====\033[0m\n"
+    printf "${CYAN}==== Running System Tools Installation Script ====${RESET}\n"
     if [ -x scripts/system_tools.sh ]; then
         sh scripts/system_tools.sh
     else
-        printf "\033[1;31mError: scripts/system_tools.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/system_tools.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to run the package installation script
 run_install_packages() {
-    printf "\033[1;36m==== Running Package Installation Script ====\033[0m\n"
+    printf "${CYAN}==== Running Package Installation Script ====${RESET}\n"
     if [ -x scripts/install_packages.sh ]; then
         sh scripts/install_packages.sh
     else
-        printf "\033[1;31mError: scripts/install_packages.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/install_packages.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to run the Xcode configuration script
 run_xcode_configuration() {
-    printf "\033[1;36m==== Running Xcode Configuration Script ====\033[0m\n"
+    printf "${CYAN}==== Running Xcode Configuration Script ====${RESET}\n"
     if [ -x scripts/configure_xcode.sh ]; then
         sh scripts/configure_xcode.sh
     else
-        printf "\033[1;31mError: scripts/configure_xcode.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/configure_xcode.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to run the git configuration setup script
 run_git_config_setup() {
-    printf "\033[1;36m==== Running Git Configuration Setup Script ====\033[0m\n"
+    printf "${CYAN}==== Running Git Configuration Setup Script ====${RESET}\n"
     if [ -x scripts/setup_git_configs.sh ]; then
         sh scripts/setup_git_configs.sh
     else
-        printf "\033[1;31mError: scripts/setup_git_configs.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/setup_git_configs.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to run the iTerm2 configuration setup script
 run_iterm2_config_setup() {
-    printf "\033[1;36m==== Running iTerm2 Configuration Setup Script ====\033[0m\n"
+    printf "${CYAN}==== Running iTerm2 Configuration Setup Script ====${RESET}\n"
     # Check if the script has execute permissions and exists
     if [ -x scripts/setup_iterm2_configs.sh ]; then
         sh scripts/setup_iterm2_configs.sh
     else
-        printf "\033[1;31mError: scripts/setup_iterm2_configs.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/setup_iterm2_configs.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to run the macOS configuration script
 run_macos_config() {
-    printf "\033[1;36m==== Running macOS Configuration Script ====\033[0m\n"
+    printf "${CYAN}==== Running macOS Configuration Script ====${RESET}\n"
     # Check if the script has execute permissions and exists
     if [ -x scripts/macos_config.sh ]; then
         sh scripts/macos_config.sh
     else
-        printf "\033[1;31mError: scripts/macos_config.sh does not have execute permissions or does not exist.\033[0m\n"
+        printf "${CYAN}Error: scripts/macos_config.sh does not have execute permissions or does not exist.${RESET}\n"
+        exit 1
+    fi
+}
+
+# Function to run the Zsh configuration setup script
+run_zsh_config_setup() {
+    printf "${CYAN}==== Running Zsh Configuration Setup Script ====${RESET}\n"
+    # Check if the script has execute permissions and exists
+    if [ -x scripts/setup_zsh_configs.sh ]; then
+        sh scripts/setup_zsh_configs.sh
+    else
+        printf "${CYAN}Error: scripts/setup_zsh_configs.sh does not have execute permissions or does not exist.${RESET}\n"
         exit 1
     fi
 }
 
 # Function to inform the user about the bootstrap completion
 end_bootstrap() {
-    printf "\n\033[1;36m==== Bootstrap Process Complete! ====\033[0m\n"
+    printf "\n${CYAN}==== Bootstrap Process Complete! ====${RESET}\n"
 }
 
 # Main function to run the bootstrap process
@@ -109,6 +125,7 @@ main() {
     run_git_config_setup
     run_iterm2_config_setup
     run_macos_config
+    run_zsh_config_setup
     end_bootstrap
 }
 
---
+++ scripts/setup_zsh_configs.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+# Colors for printing
+CYAN="\033[1;36m"
+RESET="\033[0m"
+
+# Import our symlink functions
+. scripts/symlink_functions.sh
+
+# Function to install Oh My Zsh if not already installed
+install_oh_my_zsh() {
+    local oh_my_zsh_dir="$HOME/.oh-my-zsh"
+    
+    # Check if Oh My Zsh is already installed
+    if [ -d "$oh_my_zsh_dir" ]; then
+        printf "${CYAN}Oh My Zsh is already installed.${RESET}\n"
+        return
+    fi
+    
+    printf "${CYAN}Installing Oh My Zsh...${RESET}\n"
+    if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+        printf "${CYAN}Error: Oh My Zsh installation failed.${RESET}\n"
+        exit 1
+    fi
+}
+
+# Function to symlink the .zshrc file
+symlink_zshrc() {
+    # Define the source and target paths
+    local zsh_source_path="$(pwd)/configs/zsh/.zshrc"
+    local zsh_target_path="$HOME/.zshrc"
+    
+    # Clear any broken symlinks in the target directory
+    clear_broken_symlinks "$(dirname "$zsh_target_path")"
+
+    # Create a symlink for .zshrc
+    create_symlink "$zsh_source_path" "$zsh_target_path"
+}
+
+# Main function for installing Oh My Zsh
+main() {
+    install_oh_my_zsh
+    symlink_zshrc
+}
+
+# Call the main function
+main
